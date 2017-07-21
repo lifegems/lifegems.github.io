48e53f8e46571a64b3048492f84c5ed4f9e82400
Michalik, Joshua <jsmichal@usa.wtbts.net>
2017-07-21 16:20:44 -0400

    Update the deploy script

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -3,5 +3,6 @@
 npm run ionic:build --prod
 git checkout gh-pages
 cp -r www/* ../kings/
-git commit -am "Updated dist (auto-commit)"
+git add .
+git commit -m "Updated dist (auto-commit)"
 echo "Run 'git push origin gh-pages' and then 'git checkout master'"

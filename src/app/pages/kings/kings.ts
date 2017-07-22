7da69f1e6a9d2f2a44db7f73a540818c35c2542d
Michalik <michalik@Michaliks-MacBook-Pro.local>
2017-07-21 22:42:03 -0400

    Replace slide action with slider component

--- src/app/pages/kings/kings.ts
+++ src/app/pages/kings/kings.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController, NavController } from 'ionic-angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalController, NavController, Slides } from 'ionic-angular';
 import { KingsListModal } from './kings-list/kings-list.modal';
 import { Store } from '@ngrx/store';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/filter';
 import * as KingsActions from '../../actions/kings.actions';
 import * as kingsState from '../../reducers/kings.reducer';
 
@@ -15,6 +17,7 @@ export class KingsPage implements OnInit {
   public showPriests$: Store<boolean>;
   public showProphets$: Store<boolean>;
   public showSons$: Store<boolean>;
+  @ViewChild(Slides) slides: Slides;
 
   constructor(public navCtrl: NavController, public modalCtrl: ModalController, private store: Store<kingsState.KingsState>) {
     this.kings$ = this.store.select(kingsState.getKings);
@@ -33,26 +36,22 @@ export class KingsPage implements OnInit {
     modal.present();
     modal.onDidDismiss((kingNumber) => {
       this.store.dispatch(new KingsActions.SetKingAction(kingNumber));
+      this.slides.slideTo(kingNumber - 1);
     });
   }
 
   nextKing() {
     this.store.dispatch(new KingsActions.NextAction());
+    this.slides.slideNext();
   }
 
   prevKing() {
     this.store.dispatch(new KingsActions.PrevAction());
+    this.slides.slidePrev();
   }
 
-  scroll(event) {
-    switch(event.offsetDirection) {
-      case 2:
-        this.nextKing();
-        break;
-      case 4:
-        this.prevKing();
-        break;
-    }
+  updateKingNumber(event) {
+    this.store.dispatch(new KingsActions.SetKingAction(event.realIndex + 1));
   }
 
   toggleSons() {
